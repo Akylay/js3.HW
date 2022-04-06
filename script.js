dc4bd482640c33de312bbb7e755c3e5f0b5b11b5
Akylay <akylaymusaevaa@gmail.com>
2022-04-06 15:24:54 +0300

    init

---
+++ script.js
@@ -0,0 +1,56 @@
+/*
+const num = 123
+console.log(num)
+*/
+
+/*
+const a = 10
+const b = 2
+const c = 5
+console.log(a + b + c)*/
+
+/*
+const num1 = 100
+console.log(num1 * 2)*/
+
+/*const num2 = 67
+console.log(num2 % 7)*/
+
+/*const a1 = 10
+const b1 = 2
+console.log(a1 + b1)
+console.log(a1 - b1)
+console.log(a1 * b1)
+console.log(a1 / b1)*/
+
+/*const c1 = 15
+const d1 = 2
+let result = c1 + d1
+
+console.log(result)*/
+
+/*const a2 = 30
+const b2 = 15
+const c2 = a2 + b2
+
+console.log(c2 % 4)*/
+
+/*const a3 = 17
+const b3 = 10
+const c3 = a3 + b3
+const d3 = 12
+const result3 = c3 + d3
+
+console.log(result3)*/
+
+/*const a4 = 10
+const b4 = 99
+const result4 = a4 + b4
+
+console.log(result4)*/
+
+/*const name = "лет"
+const number = 10
+const result5 = name + 10
+
+console.log(result5)*/
